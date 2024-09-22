278fa5437026c32809d22b91ab0f09030ac8384c
Roy Smart <roytsmart@gmail.com>
2024-09-22 17:44:07 -0600

    improvements to introduction

--- ccd_snr/sections/_introduction.py
+++ ccd_snr/sections/_introduction.py
@@ -9,22 +9,40 @@ Backilluminated, silicon-based image sensors such as \CCDs\ and \CMOS\ sensors
 are ubiquitous in \UV\ solar astronomy, 
 and are currently used in many of NASA's most ambitious solar missions,
 such as \AIA\ \citep{Lemen2012} and \IRIS\ \citep{DePontieu2014}.
-Despite their popularity, measuring the \QE\ of \CCDs\ with high spectral 
-resolution across the \FUV\ and \EUV\ remains difficult due to the expense of 
-calibrated, tunable sources in these wavelength ranges.
-Therefore, theoretical models fitted to sparse measurements,
-such as those developed in \citet{Stern1994}, 
-offer the best estimate of the \QE\ in the \FUV\ and \EUV.
 
+Despite their popularity,
+understanding the \QE\ and  noise statistics of these sensors in the \UV\
+is challenging due to the phenomenon of \PCC,
+where photoelectrons generated near the back surface of the sensor
+(due to the shallow penetration depth of \UV\ photons)
+recombine before they can be measured \citep{Janesick2001}.
+\PCC\ can be difficult to quantify since it depends on how the back surface
+of the sensor was prepared by the manufacturer,
+and directly measuring it is complicated by other effects such as transmission
+through the illuminated surface and charge diffusion \citep{Janesick2001}.
+In this work, we will model \PCC\ using the simple, ad-hoc method described in
+\citet{Stern1994} and investigate its effects on the noise measured by a typical
+sensor.
+
+Another source of noise which is even less understood than \PCC\ is
 Fano noise \citep{Fano1947}, 
-the unavoidable variation in the number of electrons measured per photon, 
-is even more difficult to measure and less studied than the \QE, 
-despite being predicted to have significant variations as a function of
-wavelength \citep{Santos1991,Fraser1994}.
+the unavoidable variation in the number of electrons measured per photon.
+Fano noise is routinely measured in soft X-ray regime \citep{Rodrigues2023},
+but has possibly never been measured in the \UV\ despite being predicted to
+have significant width \citep{Santos1991} and skew \citep{Fraser1994}
+variations as a function of wavelength.
+Since the Fano noise in silicon is so small,
+this work will use a very simple model of constant Fano noise,
+which still exhibits the width variations observed in \citep{Santos1991}
+since \UV\ photons produce very few photoelectrons.
 
-In this work, we explore the consequences of the \citet{Stern1994} model by
-developing an analytic expression for the \SNR\ implied by their model, and
-investigate its effects on the \IRIS\ \FUV\ spectrograph channel.
+We will use these two noise sources along with a simple absorbance calculation
+to model a silicon imaging sensor and to develop an analytic
+expression for the expected noise measured by this sensor as a function of
+the mean signal.
+This model will be applied to predict the noise measured by \AIA\ and \IRIS,
+and we will also compare our results to some unexpected noise measurements in
+\citet{Wulser2018}.
 """
     )
     return result
