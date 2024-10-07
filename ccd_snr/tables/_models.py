b8d2aa7e1d15aca1554db96da391c36bf4f00d56
Roy Smart <roytsmart@gmail.com>
2024-10-06 20:45:51 -0600

    removed oxide roughness from the model table

--- ccd_snr/tables/_models.py
+++ ccd_snr/tables/_models.py
@@ -28,7 +28,7 @@ and \citet{Heymes2020}."""
     unit = u.nm
     unit_substrate = u.um
 
-    with result.create(pylatex.Tabular("l|rrrrrr")) as tabular:
+    with result.create(pylatex.Tabular("l|rrrrr")) as tabular:
         tabular.escape = False
         tabular.add_row(
             [
@@ -37,7 +37,6 @@ and \citet{Heymes2020}."""
                 rf"$W$ ({unit:latex_inline})",
                 rf"$\delta$ ({unit:latex_inline})",
                 rf"$D$ ({unit_substrate:latex_inline})",
-                rf"oxide roughness ({unit:latex_inline})",
                 rf"substrate roughness ({unit:latex_inline})",
             ]
         )
@@ -49,7 +48,6 @@ and \citet{Heymes2020}."""
                 f"{ccd_aia.thickness_implant.to_value(unit):0.0f}",
                 f"{ccd_aia.thickness_oxide.to_value(unit):0.1f}",
                 f"{ccd_aia.thickness_substrate.to_value(unit_substrate):0.0f}",
-                f"{ccd_aia.roughness_oxide.to_value(unit):0.1f}",
                 f"{ccd_aia.roughness_substrate.to_value(unit):0.1f}",
             ]
         )
@@ -60,7 +58,6 @@ and \citet{Heymes2020}."""
                 f"{ccd.thickness_implant.to_value(unit):0.0f}",
                 f"{ccd.thickness_oxide.to_value(unit):0.1f}",
                 f"{ccd.thickness_substrate.to_value(unit_substrate):0.0f}",
-                f"{ccd.roughness_oxide.to_value(unit):0.1f}",
                 f"{ccd.roughness_substrate.to_value(unit):0.1f}",
             ]
         )
