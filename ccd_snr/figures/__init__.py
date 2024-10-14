5e22bd220cc0d924b4ef920475d2438aa987887f
Roy Smart <roytsmart@gmail.com>
2024-10-14 11:10:57 -0600

    added missing files to __init__

--- ccd_snr/figures/__init__.py
+++ ccd_snr/figures/__init__.py
@@ -4,10 +4,12 @@ Create the figures used in the article.
 
 from ._qe_effective import qe_effective
 from ._probability_measurement import probability_measurement
-from ._fano_factor_photon import fano_factor_photon
+from ._noise_photon import noise_photon
+from ._noise_electron import noise_electron
 
 __all__ = [
     "qe_effective",
     "probability_measurement",
-    "fano_factor_photon",
+    "noise_photon",
+    "noise_electron",
 ]
